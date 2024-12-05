187c715f3ad8eb375852180ba1df56017fb81c86
Quarri6343 <kaito6343@gmail.com>
2024-12-05 14:52:53 +0900

    add font

--- ParameterHUD/ParameterHUD.cpp
+++ ParameterHUD/ParameterHUD.cpp
@@ -21,6 +21,7 @@
 #include <imgui.h>
 #include <imgui_impl_win32.h>
 #include <imgui_impl_dx11.h>
+#include <fnt.h>
 
 uintptr_t baseAddress;
 HWND hwnd = nullptr;
@@ -34,7 +35,7 @@ ImGuiIO* io = nullptr;
 
 float DEFAULT_POSITION_POS_X = 50.0f;
 float DEFAULT_POSITION_POS_Y = 900.0f;
-float FIXED_POSITION_FONT_SCALE = 3.0f;
+float FIXED_FONT_SIZE = 38.0f;
 
 void mainLoop();
 
@@ -187,6 +188,21 @@ extern "C" {
 	}
 }
 
+void DrawTextWithBackground(ImVec4 textColor, ImVec4 bgColor, const char* fmt...) {
+	va_list args;
+	va_start(args, fmt);
+	char buffer[256];
+	vsnprintf(buffer, sizeof(buffer), fmt, args);
+	va_end(args);
+
+	ImVec2 text_size = ImGui::CalcTextSize(buffer);
+	ImVec2 pos = ImGui::GetCursorScreenPos();
+
+	ImDrawList* draw_list = ImGui::GetWindowDrawList();
+	draw_list->AddRectFilled(pos, ImVec2(pos.x + text_size.x, pos.y + text_size.y), ImColor(bgColor));
+	ImGui::TextColored(textColor, "%s", buffer);
+}
+
 void mainLoop() {
 	if (!init || done)
 		return;
@@ -214,9 +230,13 @@ void mainLoop() {
 	ImGui::SetNextWindowPos(defaultPos, ImGuiCond_Once);
 	ImGui::Begin("EDF hook", NULL, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoNavInputs | ImGuiWindowFlags_NoNavFocus);
 	ImDrawList* dl = ImGui::GetWindowDrawList();
-	ImGui::Text("Location = %.1f, %.1f, %.1f", xPos, yPos, zPos);
-	ImGui::Text("Ctrl+C to copy...", xPos, yPos, zPos);
-	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io->Framerate, io->Framerate);
+
+	ImVec4 yellow = ImVec4(1.0f, 1.0f, 0.0f, 1.0f);
+	ImVec4 black = ImVec4(0.0f, 0.0f, 0.0f, 0.5f);
+
+	DrawTextWithBackground(yellow, black, "Location = %.1f, %.1f, %.1f", xPos, yPos, zPos);
+	DrawTextWithBackground(yellow, black, "Ctrl+C to copy...");
+	DrawTextWithBackground(yellow, black, "Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io->Framerate, io->Framerate);
 	ImGui::End();
 
 	// Rendering
@@ -251,8 +271,7 @@ void mainProcess() {
 	ImGui_ImplWin32_Init(hwnd);
 	ImGui_ImplDX11_Init(d3dDevice, d3dContext);
 
-	//TODO:load font
-	io->FontGlobalScale = FIXED_POSITION_FONT_SCALE;
+	io->Fonts->AddFontFromMemoryCompressedTTF(robotomono_compressed_data, robotomono_compressed_size, FIXED_FONT_SIZE);
 
 	// enable Main loop
 	init = true;
--- ReadMe.md
+++ ReadMe.md
@@ -11,8 +11,9 @@ Clone this repo. Open the project with Visual Studio and build.
 
 -----------------------
 ## Credits ##
-EDF5 Damage Number Display Mod by Eiton (MIT License)
-imgui by ocornut (MIT License)
+EDF5 Damage Number Display Mod by Eiton (MIT License)  
+imgui by ocornut (MIT License)  
+Roboto Mono font (Apache License)
   
   
 EARTH DEFENSE FORCE is the registered trademark of SANDLOT and D3 PUBLISHER INC. This project is not affiliated with or endorsed by SANDLOT or D3 PUBLISHER INC in any way.
